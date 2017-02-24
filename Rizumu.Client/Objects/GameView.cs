9c701aef3cd7d368302c31c78d1d2164142d49d3
NaamloosDT <ryandejonge@outlook.com>
2017-02-24 23:26:46 +0100

    kinda done im lazy bya cya later

---
+++ Rizumu.Client/Objects/GameView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rizumu.Client
+{
+    class GameView
+    {
+        public event EventHandler Update;
+
+        public event EventHandler Draw;
+    }
+}
