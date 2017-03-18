c15933558e7bc1d4075fd5fe37ff8f6d6f1cf5e1
NaamloosDT <ryandejonge@outlook.com>
2017-03-18 18:46:05 +0100

    ranking always returns SS bc i cant

--- Rizumu/Helpers/RankGen.cs
+++ Rizumu/Helpers/RankGen.cs
@@ -14,28 +14,8 @@ namespace Rizumu
     {
         public static string generate(int hitnotes, int maxnotes)
         {
-            string rank = "d";
-            float percent = ((float)hitnotes / maxnotes) * 100;
-            if (percent == 100)
-            {
-                rank = "ss";
-            }
-            if (percent < 100)
-            {
-                rank = "s";
-            }
-            if (percent < 90)
-            {
-                rank = "a";
-            }
-            if (percent < 80)
-            {
-                rank = "b";
-            }
-            if (percent < 70)
-            {
-                rank = "c";
-            }
+            // ss, s, a, b, c, d
+            string rank = "ss";
             return rank;
         }
     }
