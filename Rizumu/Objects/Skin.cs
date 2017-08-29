83768ef56f266d4c81dfd40765e61d4e8aedfb3c
NaamloosDT <ryandejonge@outlook.com>
2017-08-30 00:10:55 +0200

    skins fix

--- Rizumu/Objects/Skin.cs
+++ Rizumu/Objects/Skin.cs
@@ -61,46 +61,45 @@ namespace Rizumu.Objects
         {
             Skin skin = new Skin();
 
-            if (File.Exists(Path.Combine(path, "button.png")))
-                skin.Button = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "button.png"), FileMode.Open));
-            else
-                skin.Button = content.Load<Texture2D>("Gui/Button");
-
-            if (File.Exists(Path.Combine(path, "buttonselected.png")))
-                skin.ButtonHover = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "buttonselected.png"), FileMode.Open));
-            else
-                skin.ButtonHover = content.Load<Texture2D>("Gui/ButtonSelected");
-
-            if (File.Exists(Path.Combine(path, "mainbackground.png")))
-                skin.MenuBackground = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "mainbackground.png"), FileMode.Open));
-            else
-                skin.MenuBackground = content.Load<Texture2D>("Backgrounds/MainBackground");
-
-            if (File.Exists(Path.Combine(path, "songbar.png")))
-                skin.SongBar = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "songbar.png"), FileMode.Open));
-            else
-                skin.SongBar = content.Load<Texture2D>("Gui/MapData");
-
-            if (File.Exists(Path.Combine(path, "note.png")))
-                skin.Note = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "note.png"), FileMode.Open));
-            else
-                skin.Note = content.Load<Texture2D>("Sprite/Note");
-
+            skin.Button = content.Load<Texture2D>("Gui/Button");
+            skin.ButtonHover = content.Load<Texture2D>("Gui/ButtonSelected");
             skin.FontBig = content.Load<SpriteFont>("Fonts/MainBig");
             skin.Font = content.Load<SpriteFont>("Fonts/Main");
             skin.FontSmall = content.Load<SpriteFont>("Fonts/MainSmall");
+            skin.MenuBackground = content.Load<Texture2D>("Backgrounds/MainBackground");
+            skin.GetReady = content.Load<Texture2D>("Backgrounds/getready");
+            skin.SongBar = content.Load<Texture2D>("Gui/MapData");
+            skin.Note = content.Load<Texture2D>("Sprite/Note");
             skin.Hit = content.Load<SoundEffect>("SoundEffects/hit");
             skin.Miss = content.Load<SoundEffect>("SoundEffects/break");
             skin.PauseOverlay = content.Load<Texture2D>("Backgrounds/PauseOverlay");
             skin.AlphaShader = content.Load<Effect>("Fx/AlphaMap");
             skin.Particle = content.Load<Texture2D>("Particles/circle");
             skin.Circle = content.Load<Texture2D>("Sprite/circle");
-            skin.GetReady = content.Load<Texture2D>("Backgrounds/getready");
+            skin.VisionUp = content.Load<Texture2D>("Backgrounds/visiontop");
+            skin.VisionDown = content.Load<Texture2D>("Backgrounds/visionbottom");
+            skin.VisionLeft = content.Load<Texture2D>("Backgrounds/visionleft");
+            skin.VisionRight = content.Load<Texture2D>("Backgrounds/visionright");
             skin.FunctionOverlay = content.Load<Texture2D>("Backgrounds/functionoverlay");
             skin.Hello = content.Load<SoundEffect>("SoundEffects/hello");
             skin.Click = content.Load<SoundEffect>("SoundEffects/click");
             skin.MouseOver = content.Load<SoundEffect>("SoundEffects/mover");
             skin.LetsGo = content.Load<SoundEffect>("SoundEffects/letsgo");
+
+            if (File.Exists(Path.Combine(path, "button.png")))
+                skin.Button = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "button.png"), FileMode.Open));
+
+            if (File.Exists(Path.Combine(path, "buttonselected.png")))
+                skin.ButtonHover = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "buttonselected.png"), FileMode.Open));
+
+            if (File.Exists(Path.Combine(path, "mainbackground.png")))
+                skin.MenuBackground = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "mainbackground.png"), FileMode.Open));
+
+            if (File.Exists(Path.Combine(path, "songbar.png")))
+                skin.SongBar = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "songbar.png"), FileMode.Open));
+
+            if (File.Exists(Path.Combine(path, "note.png")))
+                skin.Note = Texture2D.FromStream(Graphics, new FileStream(Path.Combine(path, "note.png"), FileMode.Open));
 
             return skin;
         }
