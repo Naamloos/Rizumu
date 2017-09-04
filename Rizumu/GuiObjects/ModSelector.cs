08d5361be4b68c05f6a1a28cdff021f2652fffd3
Oscar <oscarsynn@gmail.com>
2017-09-04 17:09:28 +0200

    how to use github

--- Rizumu/GuiObjects/ModSelector.cs
+++ Rizumu/GuiObjects/ModSelector.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
+using Rizumu.GameObjects;
 
 namespace Rizumu.GuiObjects
 {
@@ -28,6 +29,8 @@ namespace Rizumu.GuiObjects
         int SelectorIndex = 1;
         int height = 0;
 
+        ModCollection mods = GameData.Instance.Mods;
+
         public ModSelector(SpriteBatch sb, int screenwidth, int screenheight)
         {
             var tex = GameData.Instance.CurrentSkin.SelectorBG;
@@ -37,51 +40,32 @@ namespace Rizumu.GuiObjects
             var font = GameData.Instance.CurrentSkin.FontSmall;
             Textx = x + (int)font.MeasureString("+..").X;
             height = (int)(font.MeasureString("A").Y + 3f);
-            SpeedMultiplier = new Text(sb, font, $"Speed: {GameData.Instance.Mods.SpeedMultiplier}", Textx, height * 1, Color.White);
-            HorizontalMirror = new Text(sb, font, $"Horizontal Mirror Mode: {GameData.Instance.Mods.HorizontalMirror}", Textx, height * 2, Color.White);
-            VerticalMirror = new Text(sb, font, $"Vertical Mirror Mode: {GameData.Instance.Mods.VerticalMirror}", Textx, height * 3, Color.White);
-            AutoMode = new Text(sb, font, $"AutoMode: {GameData.Instance.Mods.Automode}", Textx, height * 4, Color.White);
-            SizeMultiplier = new Text(sb, font, $"Size: {GameData.Instance.Mods.SizeMultiplier}", Textx, height * 5, Color.White);
-            FleshLight = new Text(sb, font, $"FlashLight: {GameData.Instance.Mods.FleshLight}", Textx, height * 6, Color.White);
-            NoFail = new Text(sb, font, $"No Fail: {GameData.Instance.Mods.NoFail}", Textx, height * 7, Color.White);
-            Instafail = new Text(sb, font, $"Insta Fail: {GameData.Instance.Mods.Instafail}", Textx, height * 8, Color.White);
-            RotationMode = new Text(sb, font, $"Rotation Mode: {GameData.Instance.Mods.RotationMode}", Textx, height * 9, Color.White);
+            SpeedMultiplier = new Text(sb, font, $"Speed: {mods.SpeedMultiplier}", Textx, height * 1, Color.White);
+            HorizontalMirror = new Text(sb, font, $"Horizontal Mirror Mode: {mods.HorizontalMirror}", Textx, height * 2, Color.White);
+            VerticalMirror = new Text(sb, font, $"Vertical Mirror Mode: {mods.VerticalMirror}", Textx, height * 3, Color.White);
+            AutoMode = new Text(sb, font, $"AutoMode: {mods.Automode}", Textx, height * 4, Color.White);
+            SizeMultiplier = new Text(sb, font, $"Size: {mods.SizeMultiplier}", Textx, height * 5, Color.White);
+            FleshLight = new Text(sb, font, $"FlashLight: {mods.FleshLight}", Textx, height * 6, Color.White);
+            NoFail = new Text(sb, font, $"No Fail: {mods.NoFail}", Textx, height * 7, Color.White);
+            Instafail = new Text(sb, font, $"Insta Fail: {mods.Instafail}", Textx, height * 8, Color.White);
+            RotationMode = new Text(sb, font, $"Rotation Mode: {mods.RotationMode}", Textx, height * 9, Color.White);
             Selector = new Text(sb, font, ">", x, height * SelectorIndex, Color.Green);
-        }
-
-        public void ChangeBool(ref bool input)
-        {
-            if (input)
-                input = false;
-            else
-                input = true;
-        }
-
-        public void ChangeFloat(ref float input, bool up)
-        {
-            if (up)
-            {
-                input += 0.1f;
-            }
-            else
-            {
-                if (input > 0.1f)
-                    input -= 0.1f;
-            }
         }
 
         public void Draw()
         {
+            var keyboard = Keyboard.GetState();
+
             Back.Draw();
-            SpeedMultiplier.Content = $"Speed: {GameData.Instance.Mods.SpeedMultiplier}";
-            HorizontalMirror.Content = $"Horizontal Mirror Mode: {GameData.Instance.Mods.HorizontalMirror}";
-            VerticalMirror.Content = $"Vertical Mirror Mode: {GameData.Instance.Mods.VerticalMirror}";
-            AutoMode.Content = $"AutoMode: {GameData.Instance.Mods.Automode}";
-            SizeMultiplier.Content = $"Size: {GameData.Instance.Mods.SizeMultiplier}";
-            FleshLight.Content = $"FlashLight: {GameData.Instance.Mods.FleshLight}";
-            NoFail.Content = $"No Fail: {GameData.Instance.Mods.NoFail}";
-            Instafail.Content = $"Insta Fail: {GameData.Instance.Mods.Instafail}";
-            RotationMode.Content = $"Rotation Mode: {GameData.Instance.Mods.RotationMode}";
+            SpeedMultiplier.Content = $"Speed: {mods.SpeedMultiplier}";
+            HorizontalMirror.Content = $"Horizontal Mirror Mode: {mods.HorizontalMirror}";
+            VerticalMirror.Content = $"Vertical Mirror Mode: {mods.VerticalMirror}";
+            AutoMode.Content = $"AutoMode: {mods.Automode}";
+            SizeMultiplier.Content = $"Size: {mods.SizeMultiplier}";
+            FleshLight.Content = $"FlashLight: {mods.FleshLight}";
+            NoFail.Content = $"No Fail: {mods.NoFail}";
+            Instafail.Content = $"Insta Fail: {mods.Instafail}";
+            RotationMode.Content = $"Rotation Mode: {mods.RotationMode}";
             SpeedMultiplier.Draw();
             HorizontalMirror.Draw();
             VerticalMirror.Draw();
@@ -91,67 +75,54 @@ namespace Rizumu.GuiObjects
             NoFail.Draw();
             Instafail.Draw();
             RotationMode.Draw();
-            if (Keyboard.GetState().IsKeyPress(Keys.Up))
+            if (keyboard.IsKeyPress(Keys.Up) && SelectorIndex > 1)
             {
-                if (SelectorIndex > 1)
-                    SelectorIndex--;
+                SelectorIndex--;
             }
-            if (Keyboard.GetState().IsKeyPress(Keys.Down))
+            if (keyboard.IsKeyPress(Keys.Down) && SelectorIndex < 9)
             {
-                if (SelectorIndex < 9)
-                    SelectorIndex++;
+                SelectorIndex++;
             }
             Selector.Y = height * SelectorIndex;
             Selector.Draw();
 
-            if(SelectorIndex == 1)
+            switch (SelectorIndex)
             {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left))
-                    ChangeFloat(ref GameData.Instance.Mods.SpeedMultiplier, false);
-                if (Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeFloat(ref GameData.Instance.Mods.SpeedMultiplier, true);
-            }
-            if (SelectorIndex == 2)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.HorizontalMirror);
-            }
-            if (SelectorIndex == 3)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.VerticalMirror);
-            }
-            if (SelectorIndex == 4)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.Automode);
-            }
-            if (SelectorIndex == 5)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left))
-                    ChangeFloat(ref GameData.Instance.Mods.SizeMultiplier, false);
-                if (Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeFloat(ref GameData.Instance.Mods.SizeMultiplier, true);
-            }
-            if (SelectorIndex == 6)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.FleshLight);
-            }
-            if (SelectorIndex == 7)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.NoFail);
-            }
-            if (SelectorIndex == 8)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.Instafail);
-            }
-            if (SelectorIndex == 9)
-            {
-                if (Keyboard.GetState().IsKeyPress(Keys.Left) || Keyboard.GetState().IsKeyPress(Keys.Right))
-                    ChangeBool(ref GameData.Instance.Mods.RotationMode);
+                case 1 when keyboard.IsKeyPress(Keys.Left):
+                    if (mods.SpeedMultiplier > 0.1F)
+                        mods.SpeedMultiplier -= 0.1f;
+                    break;
+                case 1 when keyboard.IsKeyPress(Keys.Right):
+                    mods.SpeedMultiplier += 0.1f;
+                    break;
+                case 2 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.HorizontalMirror = !mods.HorizontalMirror;
+                    break;
+                case 3 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.VerticalMirror = !mods.VerticalMirror;
+                    break;
+                case 4 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.Automode = !mods.Automode;
+                    break;
+                case 5 when keyboard.IsKeyPress(Keys.Left):
+                    if (mods.SizeMultiplier > 0.1f)
+                        mods.SizeMultiplier -= 0.1f;
+                    break;
+                case 5 when keyboard.IsKeyPress(Keys.Right):
+                    mods.SizeMultiplier += 0.1f;
+                    break;
+                case 6 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.FleshLight = !mods.FleshLight;
+                    break;
+                case 7 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.NoFail = !mods.NoFail;
+                    break;
+                case 8 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.Instafail = !mods.Instafail;
+                    break;
+                case 9 when keyboard.IsKeyPress(Keys.Left) || keyboard.IsKeyPress(Keys.Right):
+                    mods.RotationMode = !mods.RotationMode;
+                    break;
             }
         }
     }
