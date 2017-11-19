20fee98adfabc367c330c2fda61aac18265e2f8e
NaamloosDT <ryandejonge@outlook.com>
2017-11-19 21:43:24 +0100

    animation manager

---
+++ Rizumu.Core/Engine/AnimationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rizumu.Core.Engine
+{
+    internal class AnimationManager
+    {
+        private static Dictionary<string, float> _values = new Dictionary<string, float>();
+        public static float GetValue(string id)
+        {
+            if (!_values.Keys.Contains(id))
+                return 0;
+            return _values[id];
+        }
+
+        public
+    }
+}
