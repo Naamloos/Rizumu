9f4a5b4a44f9ba7e3c6b6a6b36b23321b382cdb6
NaamloosDT <NaamloosDT@gmail.com>
2018-06-25 00:37:38 +0200

    Add conversion code for legacy maps

---
+++ Rizumu.Core/GameLogic/LegacyMapConverter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using Rizumu.GameLogic.Entities;
+using System.Linq;
+
+namespace Rizumu.GameLogic
+{
+	public class LegacyMapConverter
+	{
+		// Legacy maps only have one baked in difficulty level.
+		public static bool CheckMapLegacy(JObject loaded) => loaded["difficulties"].Count() > 0;
+
+		public RizumuMap ConvertLegacy(LegacyRizumuMap legacy)
+		{
+			var converted = new RizumuMap
+			{
+				ArtistName = "",
+				Author = legacy.Creator,
+				BackgroundFile = legacy.BackgroundFile,
+				Filename = legacy.FileName,
+				SongName = legacy.Name,
+				ThumbnailFile = legacy.BackgroundFile,
+			};
+
+			var diff = new RizumuDifficulty()
+			{
+				NotesDown = legacy.NotesDown,
+				NotesLeft = legacy.NotesLeft,
+				NotesRight = legacy.NotesRight,
+				NotesUp = legacy.NotesUp,
+				Name = "Normal",
+				Offset = legacy.Offset
+			};
+
+			converted.Difficulties.Add(diff);
+
+			return converted;
+		}
+	}
+}
